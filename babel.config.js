03612cad1658bc55e8f2faad90e57de61b317c2e
Cameron Pfiffer <cameron@pfiffer.org>
2025-09-24 13:09:32 -0700

    fix(web): transform import.meta for Hermes by enabling unstable_transformImportMeta in babel-preset-expo (fixes Zustand ESM import.meta error)

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          web: {
+            // Hermes (web) doesn’t support import.meta; transform it for deps like Zustand ESM
+            unstable_transformImportMeta: true,
+          },
+        },
+      ],
+    ],
     plugins: [
       [
         'module-resolver',
